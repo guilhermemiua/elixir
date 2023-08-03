eb2ddd89836ca7f1b0954e4706a20554ac338a35
Guilherme Eiti Akita Miua <guilhermemiua@gmail.com>
2023-08-02 23:03:37 -0300

    fix: make evaluate_number fn private

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -1,8 +1,8 @@
 defmodule FizzBuzz do
-  def evaluate_number(number) when rem(number, 3) === 0 and rem(number, 5) === 0, do: "fizzbuzz"
-  def evaluate_number(number) when rem(number, 3) === 0, do: "fizz"
-  def evaluate_number(number) when rem(number, 5) === 0, do: "buzz"
-  def evaluate_number(number), do: number
+  defp evaluate_number(number) when rem(number, 3) === 0 and rem(number, 5) === 0, do: "fizzbuzz"
+  defp evaluate_number(number) when rem(number, 3) === 0, do: "fizz"
+  defp evaluate_number(number) when rem(number, 5) === 0, do: "buzz"
+  defp evaluate_number(number), do: number
 
   def execute(number_list) do
     number_list
