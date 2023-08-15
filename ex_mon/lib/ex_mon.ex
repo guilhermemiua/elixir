2165c702ba25697ea020c92727c3e88b9596eafa
Guilherme Eiti Akita Miua <guilhermemiua@gmail.com>
2023-08-14 23:46:28 -0300

    feat: add agent

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -1,7 +1,15 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
+
+  @computer_game "Robotinik"
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
+  end
+
+  def start_game(player) do
+    @computer_game
+    |> create_player(:punch, :kich, :heal)
+    |> Game.start(player)
   end
 end
