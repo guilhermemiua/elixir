8337b1c39049eaf272ff715ebbffdb4f09479a0a
Guilherme Eiti Akita Miua <guilhermemiua@gmail.com>
2023-08-14 23:24:04 -0300

    feat: add create_player

--- ex_mon/lib/player.ex
+++ ex_mon/lib/player.ex
@@ -6,7 +6,7 @@ defmodule ExMon.Player do
 
   defstruct @required_keys
 
-  def build(name, move_rnd, move_avg, move_heal) do
+  def build(name, move_avg, move_rnd, move_heal) do
     %ExMon.Player{
       life: @max_life,
       move_avg: move_avg,
