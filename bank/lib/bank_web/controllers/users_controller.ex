d58f9f3cc178fa63e977196c824a2143db3a9613
guilhermemiua <guilhermemiua@gmail.com>
2024-05-05 19:16:09 -0300

    feat: add users facade

--- bank/lib/bank_web/controllers/users_controller.ex
+++ bank/lib/bank_web/controllers/users_controller.ex
@@ -1,13 +1,13 @@
 defmodule BankWeb.UsersController do
   use BankWeb, :controller
 
-  alias Bank.Users.Create
+  alias Bank.Users
   alias Bank.Users.User
 
   action_fallback BankWeb.FallbackController
 
   def create(conn, params) do
-    with {:ok, %User{} = user} <- Create.call(params) do
+    with {:ok, %User{} = user} <- Users.create(params) do
       conn
       |> put_status(201)
       |> render(:create, user: user)
